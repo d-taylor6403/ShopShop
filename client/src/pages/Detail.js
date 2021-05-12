a6d34017634505f329fb893f38f09698a8a1c620
d-taylor6403 <dannielletaylor@outlook.com>
2021-05-12 17:21:32 -0500

    cart functionality complete

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,22 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { useStoreContext } from '../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../utils/actions';
-import { Link, useParams } from "react-router-dom";
-import { useQuery } from '@apollo/react-hooks';
+import React, {useEffect, useState} from "react";
+import {useStoreContext} from '../utils/GlobalState';
+import {REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS} from '../utils/actions';
+import {Link, useParams} from "react-router-dom";
+import {useQuery} from '@apollo/react-hooks';
+import Cart from '../components/Cart';
 
-import { QUERY_PRODUCTS } from "../utils/queries";
+import {QUERY_PRODUCTS} from "../utils/queries";
 import spinner from '../assets/spinner.gif'
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
-  
-  const { id } = useParams();
+
+  const {id} = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({})
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const {loading, data} = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const {products, cart} = state;
 
   useEffect(() => {
     if (products.length) {
@@ -28,6 +29,30 @@ function Detail() {
       });
     }
   }, [products, data, dispatch, id]);
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: {...currentProduct, purchaseQuantity: 1}
+      });
+    }
+  };
+
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    });
+  };
 
   return (
     <>
@@ -47,10 +72,13 @@ function Detail() {
             <strong>Price:</strong>
             ${currentProduct.price}
             {" "}
-            <button>
+            <button onClick={addToCart}>
               Add to Cart
             </button>
-            <button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
               Remove from Cart
             </button>
           </p>
@@ -64,6 +92,7 @@ function Detail() {
       {
         loading ? <img src={spinner} alt="loading" /> : null
       }
+      <Cart />
     </>
   );
 };
